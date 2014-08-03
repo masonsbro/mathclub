010cb79657d344894fff58f8f1c2ef53d5ecd7b2
Ethan Arnold <masonsbro456@gmail.com>
2014-08-03 03:43:45 -0500

    Create user model

--- mathapp/models.py
+++ mathapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+
+	email = models.EmailField(unique = True)
+	password_hash = models.CharField(max_length = 128)
+	password_salt = models.CharField(max_length = 32)
+	reset_code = models.CharField(max_length = 32, null = True, blank = True, default = "")
+	admin = models.BooleanField(default = False)
+	contributor = models.BooleanField(default = False)
+	active = models.BooleanField(default = False)
+
+	def set_password(self, password):
+		# Generate random salt
+		salt = uuid.uuid4().hex
+		# Apply salt to password
+		salted = password + salt
+		# Hash the salted password
+		hashed = hashlib.sha512(salted).hexdigest()
+		# Store the results for later
+		self.password_hash = hashed
+		self.password_salt = salt
+
+	def check_password(self, password):
+		# Apply salt to password
+		salted = password + self.password_salt
+		# Hash the salted password
+		hashed = hashlib.sha512(salted).hexdigest()
+		# Check it against the stored one
+		return self.password_hash == hashed
--- mathapp/views.py
+++ mathapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from mathapp.models import *
+
 # Create your views here.
 
 def index(req):
