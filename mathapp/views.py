abb7381c838ebc4a7c3d282c271139837f63d165
Ethan Arnold <masonsbro456@gmail.com>
2014-08-03 03:35:12 -0500

    Start bootstrap site

--- mathapp/views.py
+++ mathapp/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def index(req):
-	return render(req, "index.html")
+	if 'email' in req.session:
+		return render(req, "dashboard.html")
+	else:
+		return render(req, "landing.html")
