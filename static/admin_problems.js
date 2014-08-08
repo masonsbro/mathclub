1fc315b40f739fdba936bcc94167b8253aa52842
Ethan Arnold <masonsbro456@gmail.com>
2014-08-08 03:45:25 -0500

    Fix autoloading trick on admin problem edit

--- static/admin_problems.js
+++ static/admin_problems.js
@@ -1,12 +1,17 @@
 $(function() {
 	$("#skill").change(function(event) {
+		var curValue = parseInt($("#learn_item option:selected").val());
 		$("#learn_item optgroup").remove();
 		$("#learn_item option:gt(0)").remove();
 		$("#learn_item").append("<option value=\"-1\">Loading tricks...</option>")
 		$.getJSON("/ajax/learn_items/skill/" + $("#skill").val() + "/", function(data) {
 			$("#learn_item option:gt(0)").remove();
 			data.forEach(function(element) {
-				$("#learn_item").append("<option value=\"" + element.pk + "\">" + element.fields.title + "</option>");
+				if (curValue == element.pk) {
+					$("#learn_item").append("<option value=\"" + element.pk + "\" selected>" + element.fields.title + "</option>");
+				} else {
+					$("#learn_item").append("<option value=\"" + element.pk + "\">" + element.fields.title + "</option>");
+				}
 			});
 		});
 	});
